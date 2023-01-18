7d5a7d9ae1391e2622cb38e916b239433f8b7e3e
Ilya Merochkin <ilya.merochkin@mail.ru>
2023-01-19 02:42:35 +0300

    Standardize error

--- internal/app/store/sqlstore/user_repository.go
+++ internal/app/store/sqlstore/user_repository.go
@@ -1,7 +1,9 @@
 package sqlstore
 
 import (
+	"database/sql"
 	"rest-api/internal/app/model"
+	"rest-api/internal/app/store"
 )
 
 type UserRepository struct {
@@ -34,6 +36,10 @@ func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 		&u.Email,
 		&u.EncryptedPassword,
 	); err != nil {
+		if err == sql.ErrNoRows {
+			return nil, store.ErrRecordNotFound
+		}
+
 		return nil, err
 	}
 
--- internal/app/store/teststore/user_repository.go
+++ internal/app/store/teststore/user_repository.go
@@ -1,8 +1,8 @@
 package teststore
 
 import (
-	"errors"
 	"rest-api/internal/app/model"
+	"rest-api/internal/app/store"
 )
 
 type UserRepository struct {
@@ -28,7 +28,7 @@ func (r *UserRepository) Create(u *model.User) error {
 func (r *UserRepository) FindByEmail(email string) (*model.User, error) {
 	u, ok := r.users[email]
 	if !ok {
-		return nil, errors.New("User not found")
+		return nil, store.ErrRecordNotFound
 	}
 
 	return u, nil
