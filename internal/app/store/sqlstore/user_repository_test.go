e551c6090c7d50ecc2249b8f0032a76b4fc11052
Ilya Merochkin <ilya.merochkin@mail.ru>
2023-01-19 02:16:56 +0300

    Refactor store

--- internal/app/store/sqlstore/user_repository_test.go
+++ internal/app/store/sqlstore/user_repository_test.go
@@ -1,26 +1,27 @@
-package store_test
+package sqlstore_test
 
 import (
 	"rest-api/internal/app/model"
-	"rest-api/internal/app/store"
+	"rest-api/internal/app/store/sqlstore"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
 func TestUserRepository_Create(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
+	s := sqlstore.New(db)
 
-	u, err := s.User().Create(model.TestUser(t))
-
-	assert.NoError(t, err)
+	u := model.TestUser(t)
+	assert.NoError(t, s.User().Create(u))
 	assert.NotNil(t, u)
 }
 
 func TestUserRepository_FindByEmail(t *testing.T) {
-	s, teardown := store.TestStore(t, databaseURL)
+	db, teardown := sqlstore.TestDB(t, databaseURL)
 	defer teardown("users")
+	s := sqlstore.New(db)
 
 	email := "user@example.org"
 	_, err := s.User().FindByEmail(email)
